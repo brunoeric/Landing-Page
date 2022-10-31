88ac9be9971243e022e86436dcd9c037b91236ba
Bruno Éric <brunosia@hotmail.com>
2022-10-31 19:56:27 -0300

    update products cards, small style changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* menu toggle */
+/* Menu Toggle */
 
 const nav = document.querySelector(
 	'#header nav');
@@ -25,68 +25,89 @@ for(const link of links) {
 
 
 
-/* Home slideshow */
 
-let time = 5000,
-		currentImageIndex = 0,
-		images = document.querySelectorAll("#slideshow img"),
-		numberOfImages = images.length;
+/* Swiper.js */
 
-function showNextImage() {
+	/* Home slideshow */
 
-	images[currentImageIndex].classList.remove("selected");
+	const swiperHome = new Swiper('#home .swiper-container', {
+		allowTouchMove: false,
+		mousewheel: false,
+		keyboard: false,
 
-	currentImageIndex++;
+		pagination: {
+			el: '#home .swiper-pagination',
+			clickable: true
+		},
 
-	if(currentImageIndex >= numberOfImages) {
-		currentImageIndex = 0;
-	}
+		effect: 'fade',
+		speed: 1500,
+		loop: true,
 
-	images[currentImageIndex].classList.add("selected");
-}
-
-function startSlideshow() {
-	setInterval( () => {
-		showNextImage()
-	}, time)
-}
-
-window.addEventListener("load", startSlideshow);
-
-
-/* Testimonials Carousel Slider from - Swiper */
-
-const swiper = new Swiper('.swiper-container', {
-	mousewheel: false,
-	keyboard: true,
-
-	loop: true,
-	slidesPerView: 1,
-
-	pagination: {
-    el: '.swiper-pagination',
-    // type: 'fraction',
-		clickable: true,
-		dynamicBullets: true,
-		dynamicMainBullets: 2
-  },
-
-	autoplay: {
-		delay: 8000,
-		disableOnInteraction: false,
-		pauseOnMouseEnter: true,
-	},
-
-	breakpoints: {
-		767: {
-			slidesPerView: 2,
-			slidesPerGroup: 2,
-			setWrapperSize: true,
-			loopFillGroupWithBlank: true,
-			grabCursor: true
+		autoplay: {
+			delay: 2000,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
 		}
-	}
-});
+	});
+
+ 	/* Products Carousel Slider */
+	
+	 const swiperProducts = new Swiper('#products .swiper-container', {
+		allowTouchMove: true,
+		mousewheel: false,
+		keyboard: true,
+
+		initialSlide: 1,
+
+		effect: 'cards',
+		cardsEffect: {
+			perSlideOffset: 75,
+			slideShadows: false,
+			perSlideRotate: 2
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
+	/* Testimonials Carousel Slider */
+
+	const swiperTestimonials = new Swiper('#testimonials .swiper-container', {
+		mousewheel: false,
+		keyboard: true,
+
+		loop: true,
+		slidesPerView: 1,
+
+		pagination: {
+			el: '#testimonials .swiper-pagination',
+			// type: 'fraction',
+			clickable: true,
+			dynamicBullets: true,
+			dynamicMainBullets: 2
+		},
+
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
+		},
+
+		breakpoints: {
+			767: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+				setWrapperSize: true,
+				loopFillGroupWithBlank: true,
+				grabCursor: true
+			}
+		}
+	});
+
+
 
 
 
@@ -97,81 +118,75 @@ window.addEventListener('scroll', function () {
 	activateMenuAtCurrentSection()
 });
 
+	/* header shadow toggle*/
 
+	const header = document.querySelector(
+		'#header');
+	const headerHeight = header.offsetHeight;
 
-/* header shadow toggle*/
-
-const header = document.querySelector(
-	'#header');
-const headerHeight = header.offsetHeight;
-
-function changeHeaderWhenScrolling() {
-	
-	if (this.window.scrollY >= headerHeight) {
-		header.classList.add('scroll');
-	} else {
-		header.classList.remove('scroll');
-	}
-}
-
-
-
-/* active nav button depending of the current site section on scroll */
-
-const sections = document.querySelectorAll(
-	'main section[id]');
-
-function activateMenuAtCurrentSection() {
-
-	const activeSessionObserverPosition = window.pageYOffset + (window.innerHeight / 8) * 4;
-
-	for (const section of sections) {
-		const sectionTop = section.offsetTop;
-		const sectionHeight = section.offsetHeight;
-		const sectionId =  section.getAttribute("id");
-
-		const isAfterTop = 
-		activeSessionObserverPosition >= 
-		sectionTop;
-
-		const isBeforeBottom = 
-		activeSessionObserverPosition <= 
-		sectionTop + sectionHeight;
-
-		if (isAfterTop && isBeforeBottom) {
-			document
-				.querySelector(`nav ul li a[href*="${sectionId}"]`)
-				.classList.add('active');
-				
+	function changeHeaderWhenScrolling() {
+		
+		if (this.window.scrollY >= headerHeight) {
+			header.classList.add('scroll');
 		} else {
+			header.classList.remove('scroll');
+		}
+	}
+
+	/* active nav button depending of the current site section on scroll */
+
+	const sections = document.querySelectorAll(
+		'main section[id]');
+
+	function activateMenuAtCurrentSection() {
+
+		const activeSessionObserverPosition = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+		for (const section of sections) {
+			const sectionTop = section.offsetTop;
+			const sectionHeight = section.offsetHeight;
+			const sectionId =  section.getAttribute("id");
+
+			const isAfterTop = 
+			activeSessionObserverPosition >= 
+			sectionTop;
+
+			const isBeforeBottom = 
+			activeSessionObserverPosition <= 
+			sectionTop + sectionHeight;
+
+			if (isAfterTop && isBeforeBottom) {
 				document
-				.querySelector(`nav ul li a[href*="${sectionId}"]`)
-				.classList.remove('active');
+					.querySelector(`nav ul li a[href*="${sectionId}"]`)
+					.classList.add('active');
+					
+			} else {
+					document
+					.querySelector(`nav ul li a[href*="${sectionId}"]`)
+					.classList.remove('active');
+			}
+
 		}
 
 	}
 
-}
+	/* show site sections with delay when scrolling - from ScrollReveal */
 
+	const scrollReveal = ScrollReveal({
+		origin: 'top',
+		distance: '30px',
+		duration: 700,
+		reset:true
+	});
 
-
-/* show site sections with delay when scrolling - from ScrollReveal */
-
-const scrollReveal = ScrollReveal({
-	origin: 'top',
-	distance: '30px',
-	duration: 700,
-	reset:true
-});
-
-scrollReveal.reveal(
-	`
-	#home .image, #home .text,
-	#about .image, #about .text,
-	#products header, #products .card,
-	#testimonials header, #testimonials .testimonials,
-	#contact .text, #contact .links,
-	footer .brand, footer .social
-	`,
-	{ interval: 150}
-);
+	scrollReveal.reveal(
+		`
+		#home .image, #home .text,
+		#about .image, #about .text,
+		#products header, #products .swiper,
+		#testimonials header, #testimonials .testimonials,
+		#contact .text, #contact .links,
+		footer .brand, footer .social
+		`,
+		{ interval: 150}
+	);
